75ed7fbf7d4fee48f7511ba231a598df33898800
Rodrigo Lins <linsprodrigo@gmail.com>
2022-08-16 10:50:33 -0300

    Add files via upload

---
+++ aula.R
@@ -0,0 +1,12 @@
+## Estudos Avançados de Metodologia de Pesquisa (PPGCP/UFPE) ##
+## Professor: Hugo Medeiros ##
+## Aluno: Rodrigo Lins ##
+
+## Criando objeto simples ##
+vetor <- c(1,2,3,5,7)
+str(vetor)
+
+## Criando objeto complexo ##
+reg1 <- lm(mpg~ ., mtcars)
+reg1
+str(reg1)
